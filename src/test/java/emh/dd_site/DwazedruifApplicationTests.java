c972e341ed6306df1f87cecaa3c5ebe1708b932a
Edwin Hageman <edwin.hageman@gmail.com>
2025-06-17 11:52:30 +0200

    initial commit

---
+++ src/test/java/emh/dd_site/DwazedruifApplicationTests.java
@@ -0,0 +1,13 @@
+package emh.dd_site;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DdSiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
