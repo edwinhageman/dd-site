79709e9acd293e23859d1e88a48c4859907bf772
Edwin Hageman <edwin.hageman@gmail.com>
2025-09-23 20:49:46 +0200

    Added springdoc openapi library for documenting backend api and generating frontend models

--- src/main/java/emh/dd_site/config/CorsConfiguration.java
+++ src/main/java/emh/dd_site/config/CorsConfiguration.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Profile("dev")
+@Profile("local")
 @Configuration
 public class CorsConfiguration implements WebMvcConfigurer {
 
