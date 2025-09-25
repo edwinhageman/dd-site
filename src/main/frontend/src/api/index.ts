ebd8021ba08242abce362fc75e4692e06aff86eb
Edwin Hageman <edwin.hageman@gmail.com>
2025-09-25 13:34:02 +0200

    Set basePath configuration property for the generated api client code to a blank value to make sure the api calls are always relative to the frontend location.

--- src/main/frontend/src/api/index.ts
+++ src/main/frontend/src/api/index.ts
@@ -1,12 +1,9 @@
 import { Configuration, EventControllerApi, WineControllerApi } from '@/generated/api'
 
 // make sure the api calls are passed through the vite development server proxy to prevent CORS issues
-let apiConfig = new Configuration()
-if (import.meta.env.MODE === 'development') {
-  apiConfig = new Configuration({
-    basePath: 'http://localhost:3000',
-  })
-}
+const apiConfig = new Configuration({
+  basePath: import.meta.env.VITE_API_BASE_URL ?? '',
+})
 
 export const eventApi = new EventControllerApi(apiConfig)
 export const wineApi = new WineControllerApi(apiConfig)
